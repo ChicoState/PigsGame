25600bdd2accae3dc17623ab34114753ff4a634b
Shirley <jx63king@live.cn>
2015-09-09 15:01:34 -0700

    Incomplete PlayerClass

---
+++ Player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream> 
+
+
+using namespace std; 
+
+class Player
+{
+  public: 
+  player(int score, int name);
+  int total();
+
+
+  private: 
+  
+
+
+
+
+};
+#endif
