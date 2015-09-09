b15c3721b716635bdd8e9047f7659a5c6de52635
egerber <emanuelgerber@web.de>
2015-09-09 12:48:53 -0700

    add boolean value to class

--- Cpu.cpp
+++ Cpu.cpp
@@ -1,15 +1,31 @@
 #include <stdlib.h> 
 
-Class Cpu
+class Cpu
 {
 	private:
 		int score;
 		int score_total;
 		int turns;
-		
+		bool holding;
+
 	public: 
+		Cpu()
+		{
+			
+		}
+
+		~Cpu()
+		{
+
+		}
+		int score_total(void)
+		{
+			return score_total;
+		}
+
 		void roll(void)
 		{
+			holding=false;
 			outcome=rand() % 6 + 1;
 			if(outcome==1)
 			{
@@ -26,6 +42,7 @@ Class Cpu
 		void hold(void)
 		{
 			score_total+=score;
+			holding=true;
 		}
 
 }
