8ad5e4af7af2caf53c0acd0dd3784fb4c9daad27
Enda Wang <ewang2@mail.csuchico.edu>
2015-09-15 02:22:33 +0800

    update
    update this file

--- computer.cpp
+++ computer.cpp
@@ -1,1 +1,22 @@
 //cpp file for computer
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int computer(int playernum, int totalscore, int score)
+{
+	if(score == 1)
+	{
+		playernum++;
+	}
+	else
+	{
+		if(totalscore >= 100)
+		{
+			cout<<"Player "<<playernum<<" is win !"<<endl;
+		}
+	}
+	return playernum
+}
+  
