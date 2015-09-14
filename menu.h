3baefcb4114ddcb4435d4d3d78d9322421f7f4ce
Jay DiFuria <jaddifuria@gmail.com>
2015-09-14 11:50:07 -0700

    Updated menu to be working, needs other classes to fully function.

--- menu.h
+++ menu.h
@@ -13,14 +13,19 @@ class Menu{
     int   getNumberOfDice();
     int   getNumberOfPlayers();
     int   getScore();
-    void  printWelcome();
+    int   printWelcome();
+    void  printInstructions();
     void  printStart();
     void  printScore();
     void  printWin();
     void  printLose();
-
+    void  quit();
   private:
-    int   DieSides;
+    int   dieSides;
+    int   numDice;
+    int   numPlayers;
+    int   score;
+
 
 };
 
