5f56c2aca314f9ddcc6223e92865734d8f3668f0
Adam J. Penn <madannep@gmail.com>
2015-09-14 11:13:35 -0700

    added define's to dice.cpp and made rand genreate correct number

--- dice.cpp
+++ dice.cpp
@@ -1,11 +1,16 @@
 #include "dice.h"
 
+#ifndef DICE_H
+#define DICE_H
+
 Dice::Dice(int size) {
   size_ = size;
   srand (time(0));
 }
 
 int Dice::Roll() {
-  int diceValue = rand() % 6;
+  int diceValue = rand() % size_ + 1;
   return diceValue;
 }
+
+#endif //DICE_H
