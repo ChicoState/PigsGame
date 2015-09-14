9fdaf950d01e111b67790e727ed845672b675d17
albertoarmienta <albertoarmienta@gmail.com>
2015-09-14 11:19:11 -0700

    main now creates a Game object to run the actual game

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
+#include<string>
+#include<game.h>
 
 using namespace std;
 int main()
 {
-  int count;
+  string name;
+  int diceSides;
+  cout<<"What is your name?"<<endl;  
+  cin>>name
   cout<<"How many sides does this Die have?"<<endl;  
-  cin>>count;
+  cin>>diceSides;
+
+  Game game(1, diceSides, name);
+  game.run();
 }
--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-PigsGame:
+PigsGame:game.o
 	g++ main.cpp -o PigsGame
 
+game.o: game.cpp game.h 
+	g++ -c game.cpp 
 clean:
 	rm PigsGame *.o
