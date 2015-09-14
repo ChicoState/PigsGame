da91a455f46f1a1da74950e9f703697e3ad19139
Jesse <jsmith189@mail.csuchico.edu>
2015-09-14 11:33:44 -0700

    Adds some loaded dice functionality

--- dice.cpp
+++ dice.cpp
@@ -8,8 +8,31 @@ Dice::Dice(int sides) {
 }
 
 int Dice::roll() {
-    return ((rand() % num_sides) + 1); // Roll dice
+    cur_roll = (rand() % num_sides);
+    cur_roll += 1; 
+    return cur_roll;
 }
+
+int Dice::roll(int target) {
+    if((target > 0) && (target < 7)) {
+        cur_roll = target;
+    } else {
+        cur_roll = 6;
+    }
+    return cur_roll;
+}
+
+int Dice::roll_no_one() {
+    cur_roll = (rand() % num_sides);
+
+    while(cur_roll == 1) {
+        cur_roll = (rand() % num_sides);
+    }
+    
+    return cur_roll;
+}
+
+
 
 int Dice::get_sides() {
     return num_sides; // Get number of sides on dice
--- dice.h
+++ dice.h
@@ -3,11 +3,14 @@
 
 class Dice {
     public:
-       Dice(int sides);
-       int roll();
-       int get_sides();
+       Dice(int);
+       int roll(); // Fair roll
+       int roll(int); // Tell dice what you want to roll
+       int roll_no_one(); // Unfair roll (cannot roll a 1)
+       int get_sides(); // Return number of sides on dice
     private:
        int num_sides;
+       int cur_roll;
 };
 
 #endif
