6a32864c40cd90261b87ce20e18e3bf872ec1206
Jacob Shouse <lordofjam@charter.net>
2015-09-14 11:32:58 -0700

    Built basic class and framework for class

--- player.cpp
+++ player.cpp
@@ -1,1 +1,23 @@
 //cpp file for player
+
+#include"player.h"
+
+using namespace std;
+
+void Player:Player()
+{
+
+}
+
+void Player:ResetTurnScore()
+{
+
+}
+void Player:CheckForContinue()
+{
+
+}
+void Player:ReturnTotalScore()
+{
+
+}
--- player.h
+++ player.h
@@ -1,1 +1,19 @@
 //Header file for Player
+#ifndef PLAYER_H;
+#define PLAYER_H;
+using namespace std;
+
+Class Player
+{
+Public:
+	Player()
+	void ResetTurnScore();
+	void CheckForContinue();
+	void ReturnTotalScore();
+
+Private:
+	int TurnScore;
+	int TotalScore;
+};
+
+#endif
