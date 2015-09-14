8c98d99c7b4ee5e5d25f442b1d2f17d1970ec3bc
Jesse <jsmith189@mail.csuchico.edu>
2015-09-14 11:22:09 -0700

    Fixes random number generation

--- dice.cpp
+++ dice.cpp
@@ -1,14 +1,16 @@
 #include "dice.h"
 #include <cstdlib>
+#include <ctime>
 
 Dice::Dice(int sides) {
-    num_sides = sides;
+    num_sides = sides; // Set number of dice sides
+    srand((unsigned)time()); // Seed random number generator
 }
 
 int Dice::roll() {
-    return rand() % num_sides;
+    return ((rand() % num_sides) + 1); // Roll dice
 }
 
 int Dice::get_sides() {
-    return num_sides;
+    return num_sides; // Get number of sides on dice
 }
