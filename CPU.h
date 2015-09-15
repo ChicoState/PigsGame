89de580beba74a8b67130354231e00dedb16d066
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-09-14 19:28:52 -0700

    Added Header Guard

--- CPU.h
+++ CPU.h
@@ -1,10 +1,14 @@
 //Edward Brown
 //PigsGame CPU Player class
+#ifndef PLAYER_H
+#define PLAYER_H
 #include "Player.h"
 
 //Note for later: implement a difficulty level
 class CPU_Player: public Player
 {
   private:
-  	bool determine_hold(int prev_roll, int cur_score, int roll_num);	
-}
+  	bool determine_hold(int prev_roll, int cur_score, int roll_num);
+};
+
+#endif
