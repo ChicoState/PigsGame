9404cb50082df3b456a24c8f9dfa3c68150cb763
Pedro Henrique <paraujosobral@mail.csuchico.edu>
2015-09-15 18:55:17 -0700

    Basic IA to decide to hold or die and name method added

--- CpuPlayer.cpp
+++ CpuPlayer.cpp
@@ -1,4 +1,6 @@
 #include "CpuPlayer.h"
+#include <chrono>
+#include <thread>
 
 CpuPlayer::CpuPlayer()
 {
@@ -16,4 +18,20 @@ bool CpuPlayer::determine_hold(int prev_roll, int cur_score, int roll_num)
  // 	IO.command(COM_HOLD);
  // }
  return false;
+}
+
+int CpuPlayer::get_command(int cur_score)
+{
+    // int cur_score = 0;// game->getCurrentPlayerScore();
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // thinking
+
+    if (cur_score < 20) {
+        return COM_ROLL;
+    }
+    return COM_HOLD;
+}
+
+std::string CpuPlayer::name()
+{
+    return "CPU";
 }
--- CpuPlayer.h
+++ CpuPlayer.h
@@ -3,6 +3,8 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Player.h"
+#include "Io.h"
+#include <string>
 
 //Note for later: implement a difficulty level
 class CpuPlayer: public Player
@@ -10,6 +12,8 @@ class CpuPlayer: public Player
 public:
     CpuPlayer();
   	bool determine_hold(int prev_roll, int cur_score, int roll_num);
+    virtual int get_command(int);
+    virtual std::string name();
 };
 
 #endif
